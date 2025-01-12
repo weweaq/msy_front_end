79b2eecc2e9359abc5c4b383f03c6a1edd9423d0
weweaq <1773465183@qq.com>
2025-01-12 21:50:16 +0800

    完成初始化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,38 @@
+"use client";
+
 import "./globals.css";
 import BasicLayout from "@/layouts/BasicLayout";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+/**
+ * 全局初始化逻辑
+ * @param children
+ * @constructor
+ */
+/**
+ * 执行初始化逻辑的布局（多封装一层）
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<
+  Readonly<{
+    children: React.ReactNode;
+  }>
+> = ({ children }) => {
+  /**
+   * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+   */
+  const doInit = useCallback(() => {
+    console.log("hello 欢迎来到我的项目");
+  }, []);
+
+  // 只执行一次
+  useEffect(() => {
+    doInit();
+  }, []);
+
+  return <>{children}</>;
+};
 
 export default function RootLayout({
   children,
@@ -11,9 +42,11 @@ export default function RootLayout({
   return (
     <html lang="zh">
       <body>
-        <AntdRegistry>
-          <BasicLayout>{children}</BasicLayout>
-        </AntdRegistry>
+        <InitLayout>
+          <AntdRegistry>
+            <BasicLayout>{children}</BasicLayout>
+          </AntdRegistry>
+        </InitLayout>
       </body>
     </html>
   );
