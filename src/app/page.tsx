7d10df911b9e8ae2c0b6770cde5f7aa8ffcc57ee
weweaq <1773465183@qq.com>
2025-01-26 21:08:31 +0800

    主页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <div>主页-根目录</div>;
+import { listQuestionBankVoByPageUsingPost } from "@/api/questionBankController";
+import { listQuestionVoByPageUsingPost } from "@/api/questionController";
+import { Divider, Flex, message } from "antd";
+import Title from "antd/lib/typography/Title";
+import Link from "next/link";
+import QuestionBankList from "@/components/QuestionBankList";
+import QuestionList from "@/components/QuestionList";
+
+export default async function HomePage() {
+  let questionBankList = [];
+  let questionList = [];
+  try {
+    const res = await listQuestionBankVoByPageUsingPost({
+      pageSize: 12,
+      sortField: "createTime",
+      sortOrder: "descend",
+    });
+    questionBankList = res.data.records ?? [];
+  } catch (e) {
+    message.error("获取题库列表失败，" + e.message);
+  }
+
+  try {
+    const res = await listQuestionVoByPageUsingPost({
+      pageSize: 12,
+      sortField: "createTime",
+      sortOrder: "descend",
+    });
+    questionList = res.data.records ?? [];
+  } catch (e) {
+    message.error("获取题目列表失败，" + e.message);
+  }
+
+  return (
+    <div id="homePage" className="max-width-content">
+      <Flex justify="space-between" align="center">
+        <Title level={3}>最新题库</Title>
+        <Link href={"/banks"}>查看更多</Link>
+      </Flex>
+      <QuestionBankList questionBankList={questionBankList} />
+      <Divider />
+      <Flex justify="space-between" align="center">
+        <Title level={3}>最新题目</Title>
+        <Link href={"/questions"}>查看更多</Link>
+      </Flex>
+      <QuestionList questionList={questionList} />
+    </div>
+  );
 }
